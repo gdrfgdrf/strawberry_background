b0901ac9d65fcea4cb19f685f13c7f436edac5ad
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2026-03-28 14:04:24 +0800

    implement Sync and Send for ServiceFfiAdapter

--- src/adapters/ffi/service_ffi_adapter.rs
+++ src/adapters/ffi/service_ffi_adapter.rs
@@ -8,6 +8,9 @@ pub struct ServiceFfiAdapter {
     runtime: Arc<ServiceRuntime>,
 }
 
+unsafe impl Sync for ServiceFfiAdapter {}
+unsafe impl Send for ServiceFfiAdapter {}
+
 impl ServiceFfiAdapter {
     pub fn new(runtime: Arc<ServiceRuntime>) -> Self {
         Self { runtime }
