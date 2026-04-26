e07f8df4e232e4ac0ca1c395272d4648d3aed218
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2026-04-27 00:41:01 +0800

    idea's shit support to git, renamed directories are not versioned

---
+++ src/domain/models/coordinator_models.rs
@@ -0,0 +1,242 @@
+use std::cmp::Ordering;
+use std::fmt::{Display, Formatter};
+use std::hash::{Hash, Hasher};
+use std::time::Duration;
+
+#[derive(Debug, thiserror::Error)]
+pub enum CoordinatorError {
+    #[error("Error Forward: {0}")]
+    ErrorForward(String),
+    #[error("Runners Are Busy")]
+    RunnersAreBusy,
+    #[error("No Snapshot Available")]
+    NoSnapshotAvailable,
+    #[error("Wait For Runner Timeout")]
+    WaitForRunnerTimeout,
+    #[error("No Runners Available")]
+    NoRunnersAvailable,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum QueuerError {
+    #[error("Error Forward: {0}")]
+    ErrorForward(String),
+    #[error("Request is discarded")]
+    RequestDiscarded,
+    #[error("Wait Timeout")]
+    WaitTimeout,
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum RunnerError {
+    #[error("Error Forward: {0}")]
+    ErrorForward(String),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum RegistryError {
+    #[error("Cannot Find Runner By Identifier: {0}")]
+    CannotFindRunnerByIdentifier(Identifier),
+    #[error("Cannot Find Post Runner By Identifier: {0}")]
+    CannotFindPostRunnerByIdentifier(Identifier),
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum DiscoverError {
+    #[error("Error Forward: {0}")]
+    ErrorForward(String),
+    #[error("Category Not Exists: {0}")]
+    CategoryNotExists(String),
+    #[error("No Available Runners Discovered")]
+    NoAvailableRunnersDiscovered,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum CategorizerError {
+    #[error("Error Forward: {0}")]
+    ErrorForward(String),
+}
+
+pub enum Priority {
+    Top { order: Option<usize> },
+    Normal { order: Option<usize> },
+    Bottom { order: Option<usize> },
+}
+
+pub enum RetryStrategy {
+    RetryImmediately {
+        max_retry: Option<usize>,
+    },
+    RetryFixed {
+        max_retry: Option<usize>,
+        delay: Duration,
+    },
+    RetryExponentialBackoff {
+        max_retry: Option<usize>,
+        initial: Duration,
+        base: f32,
+        max_delay: Duration,
+    },
+}
+
+#[derive(Clone, PartialEq)]
+pub enum RejectStrategy {
+    Discard,
+    Wait,
+}
+
+#[derive(Eq, PartialEq, Clone)]
+pub enum RunnerStatus {
+    Idle,
+    Working,
+    Busy,
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Identifier {
+    pub id: String,
+}
+
+pub struct Progress {
+    pub value: u64,
+    pub total: u64,
+}
+
+pub struct Request {
+    pub identifier: Identifier,
+    pub priority: Priority,
+    pub retry_strategy: Option<RetryStrategy>,
+    pub post_retry_strategy: Option<RetryStrategy>,
+    pub timeout: Option<Duration>,
+}
+
+pub struct CycleSnapshot {
+    pub runner_snapshots: Vec<Result<RunnerSnapshot, RunnerError>>,
+}
+
+pub struct RunnerSnapshot {
+    pub identifier: Identifier,
+    pub retry_count: Option<usize>,
+    pub progress: Option<Progress>,
+    pub status: RunnerStatus,
+}
+
+pub struct PostRunnerSnapshot {
+    pub identifier: Identifier,
+    pub retry_count: Option<usize>,
+    pub progress: Option<Progress>,
+}
+
+#[derive(Clone)]
+pub struct GlobalConfiguration {
+    pub max_retry: Option<usize>,
+}
+
+#[derive(Clone)]
+pub struct CoordinatorConfiguration {
+    pub cycle_interval: Option<Duration>,
+    pub queue_configuration: Option<QueueConfiguration>,
+}
+
+#[derive(Clone)]
+pub struct QueueConfiguration {
+    pub cycle_interval: Option<Duration>,
+    pub max_request_count: Option<usize>,
+    pub reject_strategy: Option<RejectStrategy>,
+    pub wait_for_runner_timeout: Option<Duration>,
+    pub wait_for_queue_not_empty_timeout: Option<Duration>,
+    pub wait_for_queue_not_full_timeout: Option<Duration>,
+}
+
+pub struct CategorizerConfiguration {}
+
+pub struct RunnerConfiguration {
+    pub identifier: Identifier,
+    pub accepted_categories: Option<Vec<String>>,
+}
+
+pub struct PostRunnerConfiguration {
+    pub identifier: Identifier,
+    pub accepted_categories: Option<Vec<String>>,
+}
+
+impl Priority {
+    fn variant_rank(&self) -> usize {
+        match self {
+            Priority::Top { .. } => 2,
+            Priority::Normal { .. } => 1,
+            Priority::Bottom { .. } => 0,
+        }
+    }
+
+    fn order(&self) -> Option<usize> {
+        match self {
+            Priority::Top { order } => *order,
+            Priority::Normal { order } => *order,
+            Priority::Bottom { order } => *order,
+        }
+    }
+}
+
+impl Display for Identifier {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Identifier(id={})", self.id)
+    }
+}
+
+impl PartialEq for Priority {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl Eq for Priority {}
+
+impl PartialOrd for Priority {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Priority {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let self_variant_rank = self.variant_rank();
+        let other_variant_rank = other.variant_rank();
+        match self_variant_rank.cmp(&other_variant_rank) {
+            Ordering::Equal => {
+                let self_order = self.order();
+                let other_order = other.order();
+                let self_val = self_order.unwrap_or(usize::MAX);
+                let other_val = other_order.unwrap_or(usize::MAX);
+                other_val.cmp(&self_val)
+            }
+            other_ordering => other_ordering,
+        }
+    }
+}
+
+impl PartialEq for Request {
+    fn eq(&self, other: &Self) -> bool {
+        self.identifier == other.identifier
+    }
+}
+
+impl Eq for Request {}
+
+impl PartialOrd for Request {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Request {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.priority.cmp(&other.priority)
+    }
+}
+
+impl Hash for Identifier {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state)
+    }
+}
---
+++ src/superstructure/coordinator/registry.rs
@@ -0,0 +1,353 @@
+use crate::domain::models::coordinator_models::{
+    CycleSnapshot, Identifier, RegistryError,
+};
+use crate::domain::traits::coordinator_traits::{PostRunner, Runner};
+use lazy_static::lazy_static;
+use parking_lot::RwLock;
+use std::collections::{HashMap, HashSet};
+use std::hash::{DefaultHasher, Hash, Hasher};
+use std::sync::Arc;
+
+lazy_static! {
+    static ref RUNNER_REGISTRY: RwLock<RunnerRegistry> = RwLock::new(RunnerRegistry::new());
+}
+
+pub struct RunnerRegistry {
+    /// category -> Runners
+    runners: HashMap<String, Vec<Arc<dyn Runner>>>,
+    omnipotence_runners: Vec<Arc<dyn Runner>>,
+    /// category -> Post Runners
+    post_runners: HashMap<String, Vec<Arc<dyn PostRunner>>>,
+    omnipotence_post_runners: Vec<Arc<dyn PostRunner>>,
+    cached_runners_hash: u64,
+    cached_post_runners_hash: u64,
+}
+
+impl RunnerRegistry {
+    pub fn singleton() -> &'static RwLock<RunnerRegistry> {
+        &RUNNER_REGISTRY
+    }
+
+    fn compute_runners_hash(
+        data: &HashMap<String, Vec<Arc<dyn Runner>>>,
+        omnipotence: &Vec<Arc<dyn Runner>>,
+    ) -> u64 {
+        let mut combined = 0u64;
+        omnipotence.iter().for_each(|runner| {
+            let mut hasher = DefaultHasher::new();
+            let identifier = runner.identifier();
+            identifier.hash(&mut hasher);
+            let hash = hasher.finish();
+            combined ^= hash;
+        });
+        data.values().for_each(|runners| {
+            runners.iter().for_each(|runner| {
+                let mut hasher = DefaultHasher::new();
+                let identifier = runner.identifier();
+                identifier.hash(&mut hasher);
+                let hash = hasher.finish();
+                combined ^= hash;
+            })
+        });
+
+        combined
+    }
+
+    fn compute_post_runners_hash(
+        data: &HashMap<String, Vec<Arc<dyn PostRunner>>>,
+        omnipotence: &Vec<Arc<dyn PostRunner>>,
+    ) -> u64 {
+        let mut combined = 0u64;
+        omnipotence.iter().for_each(|post_runner| {
+            let mut hasher = DefaultHasher::new();
+            let identifier = post_runner.identifier();
+            identifier.hash(&mut hasher);
+            let hash = hasher.finish();
+            combined ^= hash;
+        });
+        data.values().for_each(|post_runners| {
+            post_runners.iter().for_each(|runners| {
+                let mut hasher = DefaultHasher::new();
+                let configuration = runners.configuration();
+                let identifier = &configuration.identifier;
+                identifier.hash(&mut hasher);
+                let hash = hasher.finish();
+                combined ^= hash;
+            })
+        });
+
+        combined
+    }
+
+    pub fn new() -> Self {
+        Self {
+            runners: HashMap::new(),
+            omnipotence_runners: Vec::new(),
+            post_runners: HashMap::new(),
+            omnipotence_post_runners: Vec::new(),
+            cached_runners_hash: 0u64,
+            cached_post_runners_hash: 0u64,
+        }
+    }
+
+    pub fn update_runner_hash(&mut self) {
+        self.cached_runners_hash =
+            Self::compute_runners_hash(&self.runners, &self.omnipotence_runners);
+    }
+
+    pub fn update_post_runner_hash(&mut self) {
+        self.cached_post_runners_hash =
+            Self::compute_post_runners_hash(&self.post_runners, &self.omnipotence_post_runners);
+    }
+
+    pub fn put_runner(&mut self, runner: Arc<dyn Runner>) {
+        let empty_vec = Vec::new();
+        let accepted_categories = runner
+            .configuration()
+            .accepted_categories
+            .as_ref()
+            .unwrap_or(&empty_vec);
+        if accepted_categories.is_empty() {
+            self.omnipotence_runners.push(runner);
+            self.update_runner_hash();
+            return;
+        }
+        accepted_categories.iter().for_each(|category| {
+            let vec = self.runners.get_mut(category);
+            if vec.is_none() {
+                let mut vec = Vec::<Arc<dyn Runner>>::new();
+                vec.push(runner.clone());
+                self.runners.insert(category.clone(), vec);
+                return;
+            }
+            let vec = vec.unwrap();
+            vec.push(runner.clone());
+        });
+
+        self.update_runner_hash();
+    }
+
+    pub fn put_post_runners(&mut self, post_runner: Arc<dyn PostRunner>) {
+        let empty_vec = Vec::new();
+        let accepted_categories = post_runner
+            .configuration()
+            .accepted_categories
+            .as_ref()
+            .unwrap_or(&empty_vec);
+        if accepted_categories.is_empty() {
+            self.omnipotence_post_runners.push(post_runner);
+            self.update_post_runner_hash();
+            return;
+        }
+        accepted_categories.iter().for_each(|category| {
+            let vec = self.post_runners.get_mut(category);
+            if vec.is_none() {
+                let mut vec = Vec::<Arc<dyn PostRunner>>::new();
+                vec.push(post_runner.clone());
+                self.post_runners.insert(category.clone(), vec);
+                return;
+            }
+            let vec = vec.unwrap();
+            vec.push(post_runner.clone());
+        });
+
+        self.update_post_runner_hash();
+    }
+
+    pub fn all_runners(&self) -> Vec<&Arc<dyn Runner>> {
+        let mut result = Vec::<&Arc<dyn Runner>>::new();
+        let mut identifiers = HashSet::<&Identifier>::new();
+        self.omnipotence_runners.iter().for_each(|runner| {
+            identifiers.insert(runner.identifier());
+        });
+        self.runners.iter().for_each(|(_, runners)| {
+            runners.iter().for_each(|runner| {
+                identifiers.insert(runner.identifier());
+            })
+        });
+
+        identifiers.iter().for_each(|identifier| {
+            let runner = self.find_runner_by_identifier(identifier);
+            if runner.is_err() {
+                return;
+            }
+            let runner = runner.unwrap();
+            result.push(runner);
+        });
+
+        result
+    }
+
+    pub fn all_post_runners(&self) -> Vec<&Arc<dyn PostRunner>> {
+        let mut result = Vec::<&Arc<dyn PostRunner>>::new();
+        let mut identifiers = HashSet::<&Identifier>::new();
+        self.omnipotence_post_runners
+            .iter()
+            .for_each(|post_runner| {
+                identifiers.insert(post_runner.identifier());
+            });
+        self.post_runners.iter().for_each(|(_, post_runners)| {
+            post_runners.iter().for_each(|post_runner| {
+                identifiers.insert(post_runner.identifier());
+            })
+        });
+
+        identifiers.iter().for_each(|identifier| {
+            let post_runner = self.find_post_runner_by_identifier(identifier);
+            if post_runner.is_err() {
+                return;
+            }
+            let post_runner = post_runner.unwrap();
+            result.push(post_runner);
+        });
+
+        result
+    }
+
+    pub fn all_runner_categories(&self) -> Vec<&String> {
+        self.runners.keys().map(|key| key).collect()
+    }
+
+    pub fn all_post_runner_categories(&self) -> Vec<&String> {
+        self.post_runners.keys().map(|key| key).collect()
+    }
+
+    pub fn find_runner_by_identifier(
+        &self,
+        identifier: &Identifier,
+    ) -> Result<&Arc<dyn Runner>, RegistryError> {
+        for runner in &self.omnipotence_runners {
+            let runner_identifier = runner.identifier();
+            if runner_identifier == identifier {
+                return Ok(runner);
+            }
+        }
+
+        let runners = self.runners.values();
+        for runners in runners {
+            for runner in runners {
+                let runner_identifier = runner.identifier();
+                if runner_identifier == identifier {
+                    return Ok(runner);
+                }
+            }
+        }
+        Err(RegistryError::CannotFindRunnerByIdentifier(
+            identifier.clone(),
+        ))
+    }
+
+    pub fn find_post_runner_by_identifier(
+        &self,
+        identifier: &Identifier,
+    ) -> Result<&Arc<dyn PostRunner>, RegistryError> {
+        for post_runner in &self.omnipotence_post_runners {
+            let post_runner_identifier = post_runner.identifier();
+            if post_runner_identifier == identifier {
+                return Ok(post_runner);
+            }
+        }
+
+        let post_runners = self.post_runners.values();
+        for post_runners in post_runners {
+            for post_runner in post_runners {
+                let post_runner_identifier = post_runner.identifier();
+                if post_runner_identifier == identifier {
+                    return Ok(post_runner);
+                }
+            }
+        }
+
+        Err(RegistryError::CannotFindPostRunnerByIdentifier(
+            identifier.clone(),
+        ))
+    }
+
+    pub fn find_post_runners_by_category(
+        &self,
+        category: &String,
+    ) -> Option<Vec<&Arc<dyn PostRunner>>> {
+        let mut result = Vec::<&Arc<dyn PostRunner>>::new();
+        let mut identifiers = HashSet::<&Identifier>::new();
+        let all_post_runners = self.all_post_runners();
+
+        all_post_runners.iter().for_each(|post_runner| {
+            let empty_vec = Vec::<String>::new();
+            let identifier = post_runner.identifier();
+            let configuration = post_runner.configuration();
+            let accepted_categories = configuration
+                .accepted_categories
+                .as_ref()
+                .unwrap_or(&empty_vec);
+            if accepted_categories.is_empty() {
+                identifiers.insert(identifier);
+                return;
+            }
+            if accepted_categories.contains(category) {
+                identifiers.insert(identifier);
+            }
+        });
+        all_post_runners.iter().for_each(|post_runner| {
+            let identifier = post_runner.identifier();
+            if !identifiers.contains(identifier) {
+                return;
+            }
+            result.push(post_runner);
+        });
+
+        if result.is_empty() {
+            return None;
+        }
+        Some(result)
+    }
+
+    pub fn find_post_runners_by_categories(
+        &self,
+        categories: &Vec<String>,
+    ) -> Option<Vec<&Arc<dyn PostRunner>>> {
+        let mut result = Vec::<&Arc<dyn PostRunner>>::new();
+        let mut identifiers = HashSet::<&Identifier>::new();
+        let all_post_runners = self.all_post_runners();
+
+        all_post_runners.iter().for_each(|post_runner| {
+            let empty_vec = Vec::<String>::new();
+            let identifier = post_runner.identifier();
+            let configuration = post_runner.configuration();
+            let accepted_categories = configuration
+                .accepted_categories
+                .as_ref()
+                .unwrap_or(&empty_vec);
+            if accepted_categories.is_empty() {
+                identifiers.insert(identifier);
+                return;
+            }
+            for category in categories {
+                if !accepted_categories.contains(category) {
+                    continue;
+                }
+                identifiers.insert(identifier);
+                break;
+            }
+        });
+        all_post_runners.iter().for_each(|post_runner| {
+            let identifier = post_runner.identifier();
+            if !identifiers.contains(identifier) {
+                return;
+            }
+            result.push(post_runner);
+        });
+
+        if result.is_empty() {
+            return None;
+        }
+        Some(result)
+    }
+
+    pub fn runners_hash(&self) -> u64 {
+        self.cached_runners_hash
+    }
+
+    pub fn post_runners_hash(&self) -> u64 {
+        self.cached_post_runners_hash
+    }
+}
