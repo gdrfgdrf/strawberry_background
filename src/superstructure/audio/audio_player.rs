a10f3920fa5b5d16d3c67d6b860927ad622958dc
gdrfgdrf <gdrfgdrfgdrfgdrfgdrfgdrf@hotmail.com>
2026-06-06 16:04:50 +0800

    remove test messages

--- src/superstructure/audio/audio_player.rs
+++ src/superstructure/audio/audio_player.rs
@@ -67,7 +67,7 @@ impl RodioEngine {
             handle.abort();
         }
 
-        eprintln!("starting fft thread");
+        // eprintln!("starting fft thread");
         let cloned_sender = self.fft_sender.clone();
         *handle = Some(self.tokio_runtime.spawn(async move {
             // let mut file = OpenOptions::new()
@@ -85,7 +85,7 @@ impl RodioEngine {
                 move || Some(Arc::clone(&reader)),
                 config,
                 move |channels, sample_rate_hz| {
-                    eprintln!("sending fft data");
+                    // eprintln!("sending fft data");
                     let fft_data = FftData::new(channels, sample_rate_hz);
                     // let formatted = format!(
                     //     "[{} Hz], channel 0: {:?}, {:?}, {:?}, {:?}\n",
@@ -98,10 +98,11 @@ impl RodioEngine {
                     // let _ = file.write(&*formatted.into_bytes());
                     // let _ = file.sync_all();
 
-                    let result = cloned_sender.send(fft_data);
-                    if result.is_err() {
-                        eprintln!("send fft data error: {:?}", result);
-                    }
+                    let _ = cloned_sender.send(fft_data);
+                    // let result = cloned_sender.send(fft_data);
+                    // if result.is_err() {
+                    //     eprintln!("send fft data error: {:?}", result);
+                    // }
                 },
             );
         }));
